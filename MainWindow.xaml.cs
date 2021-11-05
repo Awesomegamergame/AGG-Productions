193979ca790ff670baf936778a72ef71e7890f4b
awesomegamergame <awesomegamergames001@gmail.com>
2021-11-05 17:11:12 -0400

    Downgrade to .netframework 4.7.2 from 4.8 and update readme.md and add some nice code stuff

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace AGG_Productions
 {
@@ -20,14 +22,27 @@ namespace AGG_Productions
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void Chaotic_Click(object sender, RoutedEventArgs e)
         {
-
+            NoGame.Visibility = Visibility.Collapsed;
+            SelectGame.Visibility = Visibility.Collapsed;
+            var dialog = new CommonOpenFileDialog();
+            dialog.Title = "Select Install Directory";
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string folder = dialog.FileName;
+                File.WriteAllText("WriteText.txt", folder);
+                Console.Write("{0}", dialog.FileName);
+                // Do something with selected folder string
+            }
         }
     }
 }
