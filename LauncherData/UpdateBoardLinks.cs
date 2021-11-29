1bf1d4b068992e23464c7df180ee43652e1bf8d3
awesomegamergame <awesomegamergames001@gmail.com>
2021-11-29 15:01:10 -0500

    Upload most of the code for launcher auto updates just need to fix the verssion.txt

--- LauncherData/UpdateBoardLinks.cs
+++ LauncherData/UpdateBoardLinks.cs
@@ -10,5 +10,7 @@ namespace AGG_Productions
     {
         public static string ChaoticBoardLink = "https://raw.githubusercontent.com/awesomegamergame/AGG-Productions/master/UpdateBoards/ChaoticUpdates.html";
         public static string ChaoticVersionLink = "https://www.dropbox.com/s/sfd89lnravmvj4j/Version.txt?dl=1";
+        public static string LauncherLink = "https://www.dropbox.com/s/27bwz9ct96qltlx/AGG%20Productions%20Temp.zip?dl=1";
+        public static string LauncherVerLink = "https://www.dropbox.com/s/m8qj7e76vxaddh3/AGG%20Productions.txt?dl=1";
     }
 }
--- LauncherUpdater/Updater.cs
+++ LauncherUpdater/Updater.cs
@@ -14,21 +14,20 @@ namespace AGG_Productions.LauncherUpdater
 {
     class Updater
     {
-        public static string versionFile = Path.Combine(Directory.GetCurrentDirectory(), "AGG-Productions-Version.txt");
+        public static string versionFile = Path.Combine(Directory.GetCurrentDirectory(), "AGG Productions Version.txt");
         public static string rootPath = Directory.GetCurrentDirectory();
         public static string gameZip = Path.Combine(rootPath, "AGG Productions Temp.zip");
-        public static string startPath = @".\Chaotic Launcher Temp";
-        public static string gameExe = Path.Combine(rootPath, "Updater.bat");
+        public static string startPath = @".\AGG Productions Temp";
         public static void LauncherUpdate()
         {
             if (File.Exists(versionFile))
             {
-                VersionChecker.Version localVersion = new VersionChecker.Version(File.ReadAllText(versionFile));
+                Version localVersion = new Version(File.ReadAllText(versionFile));
 
                 try
                 {
                     WebClient webClient = new WebClient();
-                    VersionChecker.Version onlineVersion = new VersionChecker.Version(webClient.DownloadString("https://www.dropbox.com/s/mb1q445h8y1vgk5/ChaoticLauncherVersion.txt?dl=1"));
+                    Version onlineVersion = new Version(webClient.DownloadString(UpdateBoardLinks.LauncherVerLink));
 
                     if (onlineVersion.IsDifferentThan(localVersion))
                     {
@@ -47,11 +46,11 @@ namespace AGG_Productions.LauncherUpdater
             }
             else
             {
-                InstallGameFiles(false, VersionChecker.Version.zero);
+                InstallGameFiles(false, Version.zero);
             }
         }
 
-        private static void InstallGameFiles(bool _isUpdate, VersionChecker.Version _onlineVersion)
+        private static void InstallGameFiles(bool _isUpdate, Version _onlineVersion)
         {
             try
             {
@@ -63,11 +62,11 @@ namespace AGG_Productions.LauncherUpdater
                 else
                 {
                     //Status = LauncherStatus.downloadingLauncher;
-                    _onlineVersion = new VersionChecker.Version(webClient.DownloadString("https://www.dropbox.com/s/mb1q445h8y1vgk5/ChaoticLauncherVersion.txt?dl=1"));
+                    _onlineVersion = new Version(webClient.DownloadString(UpdateBoardLinks.LauncherVerLink));
                 }
 
                 webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
-                webClient.DownloadFileAsync(new Uri("https://www.dropbox.com/s/5qofh8awjh7pkc2/Chaotic%20Launcher%20Temp.zip?dl=1"), gameZip, _onlineVersion);
+                webClient.DownloadFileAsync(new Uri(UpdateBoardLinks.LauncherLink), gameZip, _onlineVersion);
             }
             catch (Exception ex)
             {
@@ -80,7 +79,7 @@ namespace AGG_Productions.LauncherUpdater
         {
             try
             {
-                string onlineVersion = ((VersionChecker.Version)e.UserState).ToString();
+                string onlineVersion = ((Version)e.UserState).ToString();
                 if (Directory.Exists(startPath))
                 {
                     Directory.Delete(startPath, true);
@@ -89,26 +88,53 @@ namespace AGG_Productions.LauncherUpdater
                     File.WriteAllText(versionFile, onlineVersion);
                     //VersionText.Text = onlineVersion;
                     //Status = LauncherStatus.ready;
-                    ProcessStartInfo startInfo = new ProcessStartInfo(gameExe)
-                    {
-                        WorkingDirectory = Path.Combine(rootPath, "Chaotic Development Launcher Temp")
-                    };
-                    Process.Start(startInfo);
-                    Application.Current.Shutdown();
+
+                    //Application.Current.Shutdown();
                 }
                 else
                 {
-                    ZipFile.ExtractToDirectory(gameZip, rootPath);
+                    File.Move(@".\AGG Productions.exe", @".\AGG Productions.exe.old");
+                    File.Move(@".\AGG Productions.pdb", @".\AGG Productions.pdb.old");
+                    try
+                    {
+                        //Declare a temporary path to unzip your files
+                        string tempPath = Path.Combine(Directory.GetCurrentDirectory(), "tempUnzip");
+                        string extractPath = Directory.GetCurrentDirectory();
+                        ZipFile.ExtractToDirectory(gameZip, tempPath);
+
+                        //build an array of the unzipped files
+                        string[] files = Directory.GetFiles(tempPath);
+
+                        foreach (string file in files)
+                        {
+                            FileInfo f = new FileInfo(file);
+                            //Check if the file exists already, if so delete it and then move the new file to the extract folder
+                            if (File.Exists(Path.Combine(extractPath, f.Name)))
+                            {
+                                File.Delete(Path.Combine(extractPath, f.Name));
+                                File.Move(f.FullName, Path.Combine(extractPath, f.Name));
+                            }
+                            else
+                            {
+                                File.Move(f.FullName, Path.Combine(extractPath, f.Name));
+                            }
+                        }
+                        //Delete the temporary directory.
+                        Directory.Delete(tempPath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                    //ZipFile.ExtractToDirectory(gameZip, rootPath);
                     File.Delete(gameZip);
+                    MessageBox.Show(onlineVersion);
                     File.WriteAllText(versionFile, onlineVersion);
                     //VersionText.Text = onlineVersion;
                     //Status = LauncherStatus.ready;
-                    ProcessStartInfo startInfo = new ProcessStartInfo(gameExe)
-                    {
-                        WorkingDirectory = Path.Combine(rootPath, "Chaotic Development Launcher Temp")
-                    };
-                    Process.Start(startInfo);
-                    Application.Current.Shutdown();
+
+
+                    //Application.Current.Shutdown();
 
                 }
             }
@@ -116,6 +142,64 @@ namespace AGG_Productions.LauncherUpdater
             {
                 //Status = LauncherStatus.failed;
                 MessageBox.Show($"Error finishing download: {ex}");
+            }
+        }
+        public struct Version
+        {
+            internal static Version zero = new Version(0, 0, 0);
+
+            private readonly short major;
+            private readonly short minor;
+            private readonly short subMinor;
+
+            internal Version(short _major, short _minor, short _subMinor)
+            {
+                major = _major;
+                minor = _minor;
+                subMinor = _subMinor;
+            }
+            internal Version(string _version)
+            {
+                string[] _versionStrings = _version.Split('.');
+                if (_versionStrings.Length != 4)
+                {
+                    major = 0;
+                    minor = 0;
+                    subMinor = 0;
+                    return;
+                }
+
+                major = short.Parse(_versionStrings[0]);
+                minor = short.Parse(_versionStrings[1]);
+                subMinor = short.Parse(_versionStrings[2]);
+            }
+
+            internal bool IsDifferentThan(Version _otherVersion)
+            {
+                if (major != _otherVersion.major)
+                {
+                    return true;
+                }
+                else
+                {
+                    if (minor != _otherVersion.minor)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        if (subMinor != _otherVersion.subMinor)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            public override string ToString()
+            {
+                return $"{major}.{minor}.{subMinor}";
             }
         }
     }
