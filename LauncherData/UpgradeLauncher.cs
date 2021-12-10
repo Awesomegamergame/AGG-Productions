e67eb2f3ddc55e96ce2c1b1478ed0c732b8697a3
Awesomegamergame <awesomegamergames001@gmail.com>
2021-12-09 20:31:28 -0500

    Remove Unneeded References

--- LauncherData/UpgradeLauncher.cs
+++ LauncherData/UpgradeLauncher.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
 using System.IO;
-using System.Windows;
+using System.Diagnostics;
 
 namespace AGG_Productions.LauncherData
 {
