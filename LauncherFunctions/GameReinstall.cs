55ebacbcb2a6f157fa4553792703c57e2f8e9bc5
awesomegamergame <awesomegamergames001@gmail.com>
2022-02-02 10:35:37 -0500

    Fixed a bug

--- LauncherFunctions/GameReinstall.cs
+++ LauncherFunctions/GameReinstall.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using AGG_Productions.LauncherData;
+﻿using AGG_Productions.LauncherData;
 
 namespace AGG_Productions.LauncherFunctions
 {
@@ -10,6 +9,7 @@ namespace AGG_Productions.LauncherFunctions
             MainWindow.GameReInstallObject.IsEnabled = false;
             MainWindow.button = MainWindow.GameReInstallObject;
             AdminDirCheck.InstallDir(MainWindow.InstallGameName);
+            MainWindow.GameReInstallObject.IsEnabled = true;
             MainWindow.GameDir = Json.ReadJson(MainWindow.InstallGameName);
         }
     }
