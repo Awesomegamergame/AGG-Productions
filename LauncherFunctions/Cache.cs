a270140dca2874f22e570ed7061a71191444f41d
awesomegamergame <awesomegamergames001@gmail.com>
2022-02-07 09:27:25 -0500

    Change to master branch merge time

--- LauncherFunctions/Cache.cs
+++ LauncherFunctions/Cache.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using static System.Environment;
-using System.Threading.Tasks;
 
 namespace AGG_Productions.LauncherFunctions
 {
