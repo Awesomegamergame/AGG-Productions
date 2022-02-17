cceae3c814187bf22a801ce7425075150621873f
awesomegamergame <awesomegamergames001@gmail.com>
2022-02-17 08:32:31 -0500

    Added an Argument

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,36 +1,27 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Windows;
+using AGG_Productions.LauncherFunctions;
 
 namespace AGG_Productions
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-        public static string GameName;
-        public static int ArgumentNumber;
-        //1 = AntiCheat
         private void App_Startup(object sender, StartupEventArgs e)
         {
             string[] args = Environment.GetCommandLineArgs();
             if(args.Length == 3)
-                ProcessArguments(args[1], args[2]);
-            Process[] pname = Process.GetProcessesByName(AppDomain.CurrentDomain.FriendlyName.Remove(AppDomain.CurrentDomain.FriendlyName.Length - 4));
-            if (pname.Length > 1)
             {
-                pname.Where(p => p.Id != Process.GetCurrentProcess().Id).First().Kill();
+                Arguments.ProcessArguments(args[1], args[2]);
+                Process[] pname = Process.GetProcessesByName(AppDomain.CurrentDomain.FriendlyName.Remove(AppDomain.CurrentDomain.FriendlyName.Length - 4));
+                if (pname.Length > 1)
+                {
+                    pname.Where(p => p.Id != Process.GetCurrentProcess().Id).First().Kill();
+                }
             }
             MainWindow window = new MainWindow();
             window.Show();
-        }
-        public void ProcessArguments(string arg1, string arg2)
-        {
-            
         }
     }
 }
---
+++ LauncherFunctions/Arguments.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace AGG_Productions.LauncherFunctions
+{
+    class Arguments
+    {
+        public static void ProcessArguments(string arg1, string arg2)
+        {
+            switch (arg1)
+            {
+                case "AntiCheat":
+                    foreach (Process process in Process.GetProcessesByName(arg2))
+                    {
+                        process.Kill();
+                    }
+                    MessageBox.Show($"{arg2}'s Anti-Cheat has detected that you are cheating and has closed the game.", "AGG Productions");
+                    break;
+                default:
+                    MessageBox.Show("Invalid Argument");
+                    break;
+            }
+        }
+    }
+}
